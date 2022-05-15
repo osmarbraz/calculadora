8de0730122d20a8fc4fff14160bc77840fe5e1a0
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2022-05-15 12:08:29 -0300

    Inicío do projeto

---
+++ src/test/java/calculadora/TestCalculadora.java
@@ -0,0 +1,22 @@
+package calculadora;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class TestCalculadora {
+    
+    @Test
+    public void testGetAdicao() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 6.0;
+        double retornoFeito = calculadora.getAdicao();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+
+    @Test
+    public void testGetSubtracao() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 2.0;
+        double retornoFeito = calculadora.getSubtracao();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+}
+
